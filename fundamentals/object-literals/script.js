3d6fa094c0a8eb6c8b47460e5654d131cef55ef1
Ubaid <93278128+Ubaid234@users.noreply.github.com>
2022-04-03 17:55:47 -0700

    Add files via upload

---
+++ fundamentals/object-literals/script.js
@@ -0,0 +1,40 @@
+const person = {
+    firstname: 'Steve',
+    lastname: 'Smith',
+    age: 30,
+    email : 'steve@aol.com',
+    hobbies: ['music', 'sports'],
+    address: {
+        city: 'Miami',
+        state: 'FL'
+    },
+    getBirthYear: function(){
+        return 2017 - this.age;
+    }
+}
+
+let val;
+
+val = person;
+
+// Get specied value 
+val = person.firstname; //Recommended
+val = person['firstname'];
+val = person.age;
+val = person.hobbies[1];
+val = person.address.state;
+val = person.address['city'];
+val = person.getBirthYear();
+
+
+console.log(val);
+
+const people = [
+    {name: 'John', age: 30},
+    {name: 'Mike', age : 23},
+    {name: 'Nancy', age : 40}
+];
+
+for(let i = 0; i < people.length; i++ ){
+    console.log(people[i].name);
+}
