ab3ac7e4be7945974c861458c6bc7e5a79f590c2
Ubaid <93278128+Ubaid234@users.noreply.github.com>
2022-04-11 03:49:02 -0700

    Add files via upload

---
+++ fundamentals/A-look-at-the-window-object/app.js
@@ -0,0 +1,59 @@
+// WINDOW METHODS / OBJECTS / PROPERTIES
+
+// ALERT 
+// window.alert('Hello World');
+
+// PROMT
+// const input = prompt();
+// alert(input);
+
+// CONFIRM
+// if(confirm('Are you sure')){
+//     console.log('YES');
+// } else {
+//     console.log('NO')
+// }
+
+let val;
+
+// OUTER HEIGHT AND WIDTH
+val = window.outerHeight;
+val = window.outerWidth;
+
+// INNER HEIGHT AND WIDTH
+val = window.innerHeight;
+val = window.innerWidth;
+
+// SCROLL POINTS
+val = window.scrollY;
+val = window.scrollX;
+
+// LOCATION OBJECT
+val = window.location;
+val = window.location.hostname;
+val = window.location.port;
+val = window.location.href;
+val = window.location.search;
+
+// REDIRECT
+// window.location.href = 'https://google.com';
+//RELOAD
+// window.location.reload();
+
+// HISTORY OBJECT
+// window.history.go();
+// window.history.length();
+
+// Navigator Object
+val = window.navigator
+val = window.navigator.appName;
+val = window.navigator.appVersion;
+val = window.navigator.userAgent;
+val = window.navigator.platform;
+val = window.navigator.vendor;
+val = window.navigator.language;
+
+
+
+
+console.log(val);
