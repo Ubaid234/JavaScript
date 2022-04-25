94aa6bc0adc429cd6be2775a074a254ecd46e693
ubaid <mrubaid56@gmail.com>
2022-04-25 16:49:41 +0530

    initial commit

---
+++ fundamentals/functions/app.js
@@ -0,0 +1,23 @@
+// Funtions
+
+function logger(){
+    console.log('My name is jonas');
+}
+
+//calling / running / invoking funtion
+
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples,orages) {
+    const juice = `juice with ${apples} apples and ${orages} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(4, 0);
+console.log(appleJuice);
+// console.log(fruitProcessor(4, 0));
+
+const appleOrangeJuice = fruitProcessor(2,9);
+console.log(appleOrangeJuice);
