6f89b86aecc5c0f8d9875dc545ccede1f84a4d06
ubaid <mrubaid56@gmail.com>
2022-05-06 23:29:47 +0530

    Learned  Function Arguments

---
+++ fundamentals/functions/script.js
@@ -0,0 +1,39 @@
+function rolldie () {
+    let roll = Math.floor(Math.random()  * 4) + 1;
+    console.log (`Rolled: ${roll}`);
+}
+
+// function throwDice () {
+//     rolldie();
+//     rolldie();
+//     rolldie();
+//     rolldie();
+//     rolldie();
+//     rolldie();
+// }
+
+
+// throwDice();
+
+// function throwDice(numRolls) {
+//     for ( let i = 0; i < numRolls; i++) {
+//         rolldie();
+//     }
+// }
+
+// function greet (nickname) {
+//     console.log(nickname);
+//     console.log('Hi');
+// }
+
+function square (num) {
+    console.log(num * num);
+}
+
+function sum ( x, y) {
+    console.log(x + y );
+}
+
+function divide ( a, b) {
+    console.log ( a/b);
+}
