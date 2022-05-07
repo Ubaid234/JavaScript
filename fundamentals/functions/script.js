ed3f319f7ea958800ecd26291a1501fb0ab0d61d
ubaid <mrubaid56@gmail.com>
2022-05-08 00:58:58 +0530

    Function as Return Value

--- fundamentals/functions/script.js
+++ fundamentals/functions/script.js
@@ -242,4 +242,29 @@ function pickOne (f1,f2) {
     }
 }
 
-pickOne(cry, rage);
+// pickOne(cry, rage);
+
+//  FUNCTION AS RETURN VALUES
+
+// function multiplyBy(num) {
+//     return function(x) {
+//         return x * num;
+//     }
+// }
+
+// const triple = multiplyBy(3);
+// const double = multiply(2);
+// const halve = multiplyBy(.5);
+
+
+function makeBetweenFunc (x,y) {
+    return function (num) {
+        return num >= x && num <= y;
+    }
+}
+
+const isChild = makeBetweenFunc(0,18);
+
+const isInNineties = makeBetweenFunc(1990, 1999);
+
+const isNiceWeather = makeBetweenFunc (60,79);
