2b9918f9aab00e74393d72481f4e10ba2dbc0a92
ubaid <mrubaid56@gmail.com>
2022-05-09 23:22:43 +0530

    filer()

--- fundamentals/arrays-arraymethods/script.js
+++ fundamentals/arrays-arraymethods/script.js
@@ -76,3 +76,14 @@ const caps = texts.map(function(t){
 })
 
 
+// FILTER()
+
+const nums = [34,35,67,54,109,102,32,9];
+
+const odds = nums.filter(n => n % 2 === 1);
+const evens = nums.filter(n => n % 2 === 0);
+
+const bigNums = nums.filter(n => n > 50);
+
+
+
