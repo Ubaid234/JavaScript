6782cc9dd4b8c75257e78614a087f9e498398c17
ubaid <mrubaid56@gmail.com>
2022-05-16 01:19:32 +0530

    Implementing the Game logic

--- Dom-manipulation/app.js
+++ Dom-manipulation/app.js
@@ -15,17 +15,49 @@ console.log(document.querySelector('.guess').value);
 
 */
 
+// Handling Click Elements
+// Implementing the Game Logic
+
+
+const secretNumber = Math.trunc(Math.random() * 20 + 1);
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function() {
 
     const guess = Number(document.querySelector('.guess').value);
     console.log(guess , typeof guess);
 
+    // When there is no input
     if (!guess) {
-        document.querySelector('.message').textContent = '⛔ No Number!';
-    }
+        document.querySelector('.message').textContent = '⛔ No Number!';   
+    } 
+    // When Player Wins
+    else if ( guess === secretNumber) {
+        document.querySelector('.message').textContent = '🎉 Correct Number!';
+    } 
+    // When guess is too high 
+    else if (guess > secretNumber) {
+        if (score > 1 ) {
+            document.querySelector('.message').textContent = '📈 Too High';
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = '💥 You  lost the Game';
+            document.querySelector('.score').textContent = 0 ;
+        }
+       
+    } 
+    // When guess is too low
+    else if ( guess < secretNumber) {
 
+        if (score > 1 ) {
+            document.querySelector('.message').textContent = '📈 Too High';
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = '💥 You  lost the Game';
+            document.querySelector('.score').textContent = 0 ;
+        }       
+    } 
 });
-
-
-
