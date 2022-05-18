6a37457aa5a95cfcc148fce572dfe53ca3f20348
ubaid <mrubaid56@gmail.com>
2022-05-18 22:56:12 +0530

    Altering Styles

--- Dom-Manipulation/app.js
+++ Dom-Manipulation/app.js
@@ -1,5 +1,6 @@
 // Changing Multiple Element 
 
+/*
 const allLis = document.querySelectorAll('li');
 
 for (let i = 0; i < allLis.length; i++){
@@ -8,4 +9,16 @@ for (let i = 0; i < allLis.length; i++){
 
 for (let li of allLis) {
     li.innerHTML = 'we ar <b> The ChamPIONS </b>'
-}
+}  
+*/
+
+// Altering Styles
+
+const allLis = document.querySelectorAll('li');
+
+const colors = ['red','orange','yellow','green','blue','purple'];
+
+allLis.forEach((li, i) => {
+    const color = colors[i];
+    li.style.color = color;
+})
